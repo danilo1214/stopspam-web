a5da11fbf745675208d1a64f37910d0a0ac3d7c5
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2025-02-02 16:41:58 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,6 +26,6 @@ export default withAuth({
 // This matcher applies the middleware to all routes except for /get-started and /api/auth (if you use NextAuth API routes).
 export const config = {
   matcher: [
-    "/((?!get-started|api/auth|_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|get-started|api/auth).*)",
   ],
 };
